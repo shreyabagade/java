8746dd69964c10291966651418af8d92c388f2b0
Shreya Bagade <111302123+shreyabagade@users.noreply.github.com>
2023-08-22 03:06:30 -0700

    Add files via upload

---
+++ Table.java
@@ -0,0 +1,11 @@
+class Table
+{
+    public static void main(String []args)
+    {
+        System.out.println("My First Java Program.");
+        int num =9;
+        for(int i=1;i<=10;i++){
+        System.out.println(9+"x"+i+"="+9*i);
+    }
+    }
+};
